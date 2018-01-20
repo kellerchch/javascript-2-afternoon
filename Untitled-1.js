d023aeb928bbf3edc8d18a798c687aba26b1a53a
kellerchch <kellerchch@gmail.com>
2018-01-20 13:51:51 -0700

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,13 @@
+var user1 = {
+    name: 'Zach',
+    age: 24,
+    email: 'zach@gmail.com',
+    favoriteAnimal: 'tiger',
+    lovesCode: false
+}
+
+delete user1.lovesCode
+
+user1.hasBeenSkiing = false;
+
+user1
